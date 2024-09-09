a69d91e12ce18390d0c6750119805ee1dc1d658a
ProtMello <eitanmello@outlook.com>
2024-09-09 21:17:33 +0000

    More

--- dataclass_main.py
+++ dataclass_main.py
@@ -47,11 +47,54 @@ you = DNDCharacter(
 )
 
 x = open("foo.json", "w")
-print(you.name)
+# print(you.name)
 
-you.race = "Guman"
-you.skills.append("Iron Fist")
-you.stats.strength = 12
+# you.race = "Guman"
+# you.skills.append("Iron Fist")
+# you.stats.strength = 12
 
-you = you.to_dict()
-json.dump(you, x, indent=4)
+x = """
+{
+   "armor_class": 13,
+   "class": "Monk",
+   "health": {
+      "base": 11,
+      "current": 10,
+      "temp": 5
+   },
+   "name": "HARK",
+   "proficiencies": [   
+      "Common",
+      "Short Swords",
+      "Daggers",
+      "Orcish",
+      "Tuba"
+   ],
+   "race": "Half-Orc",
+   "skills": [
+      "Unarmored Strike",
+      "Flurry of Blows",
+      "Dash",
+      "High Jump"
+   ],
+   "stats": {
+      "charisma": 12,
+      "constitution": 16,
+      "dexterity": 16,
+      "intellect": 12,
+      "strength": 10,
+      "wisdom": 14
+   }
+}
+"""
+
+#d = json.load(open("ddd.json", "r"))
+d["race"] = "Gumann"
+d["skills"].append("Irony Fist")
+d["stats"]["strength"] = 20
+
+
+# you = you.to_dict()
+# json.dump(you, x, indent=4)
+
+json.dump(d, x, indent=4)
